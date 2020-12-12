f45c5ea8836408146964fca463b287d28ee8ed44
Haley Halsey <haleyhalsey@yahoo.com>
2020-12-11 20:56:14 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+    var currentDate = moment().format('MMMM Do YYYY');
+    var currentTime = moment().format('h:mm:ss a');
+      $("#displayMoment").append(currentDate + " " + currentTime);
+    var currentHour = moment().format("h");
+})
+
+var eventTimeArr = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+ 
+
+ console.log(each);
+
+
+
+
